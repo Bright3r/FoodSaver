3f8468b9002317d3f9217fdd51f43dbbb1cdc153
SchuylerNg <schuylerng818@gmail.com>
2025-04-07 20:10:43 -0700

    Made UI adjustments to ingredient page

--- frontend/app/ingredient.tsx
+++ frontend/app/ingredient.tsx
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useSession } from './ctx';
-import { View, Text, StyleSheet, Image, Button } from 'react-native';
+import { 
+    View, 
+    Text, 
+    TextInput, 
+    StyleSheet, 
+    Image, 
+    Button, 
+    Keyboard,
+    Platform,
+    Dimensions, 
+    KeyboardAvoidingView } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
+import Constants from 'expo-constants';
+import { ScrollView } from 'react-native-gesture-handler';
 
 interface Ingredient {
     name:string;
@@ -29,8 +41,20 @@ const fetchIngredientData = async (productCode: number): Promise<Ingredient | nu
             return null;
         }
     } catch (error) {
-        console.error("Error: fetch failed.", error);
+        console.error("Error: item fetch failed.", error);
         return null;
+    }
+};
+
+
+// IP seems to be having issues. Implement this function later.
+const saveIngredient = async(username:string): Promise<void> => {
+    try {
+        const uri =
+            Constants.expoConfig?.hostUri?.split(':').shift()?.concat(':8081') ??
+            '192.168.0.44:8083';
+    } catch (error) {
+        console.error("Error: item failed to save.", error);
     }
 };
 
@@ -39,6 +63,8 @@ export default function IngredientPage() {
     const [ingredient, setIngredient] = useState<Ingredient | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
     const { scannedData } = useLocalSearchParams();
+    const [itemName, setItemName] = useState('');
+    const [itemDesc, setItemDesc] = useState('');
     const parsedScannedData = scannedData ? parseInt(scannedData as string) : NaN;
     const router = useRouter();
     // Add user inventory funct here.
@@ -60,16 +86,75 @@ export default function IngredientPage() {
                 <Text style={styles.text}>Loading...</Text>
             ) : ingredient ? ( // Include more info..?
                 <>
-                    <Text style={styles.name}>{ingredient.name}</Text>
-                    <Image source={{ uri: ingredient.imageUrl }} style={styles.image} />
-                    <Text style={styles.description}>{ingredient.description}</Text>
-                    <Text style={styles.nutritionGrade}>{ingredient.nutritionGrade}</Text>
-                    <Button title="Back" onPress={() => router.back()}/>
+                    <KeyboardAvoidingView 
+                        style={styles.nameContainer}
+                        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    >
+                        <TextInput 
+                            style={styles.name}
+                            placeholder="Ingredient name"
+                            onChangeText={val => setItemName(val)}
+                            multiline
+                            submitBehavior="blurAndSubmit"
+                        >
+                            {ingredient.name}
+                        </TextInput>
+                    </KeyboardAvoidingView>
+                    <KeyboardAvoidingView 
+                        style={styles.imageContainer}
+                        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                        keyboardVerticalOffset={Platform.OS === 'ios' ? 25 : 0}
+                    >
+                        <Image source={{ uri: ingredient.imageUrl }} style={styles.image} />
+                    </KeyboardAvoidingView>
+                    <KeyboardAvoidingView 
+                        style={styles.descriptionContainer}
+                        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                        keyboardVerticalOffset={Platform.OS === 'ios' ? 25 : 0}
+                    >
+                        <TextInput 
+                            style={styles.description}
+                            onChangeText={val => setItemDesc(val)}
+                            multiline={true}
+                            textAlignVertical='top'
+                            submitBehavior='blurAndSubmit'
+                            placeholder="Ingredient description"
+                        >
+                            {ingredient.description}
+                        </TextInput>
+                    </KeyboardAvoidingView>
+                    <View 
+                        style={styles.nutritionGradeContainer}
+                    >
+                        <Text style={styles.nutritionGrade}>{ingredient.nutritionGrade}</Text>
+                    </View>
+                    <View
+                        style={styles.buttonContainer}
+                    >
+                        <Text
+                            style={styles.button}
+                            onPress={() => {
+                                // Implement inventory functionality.
+                                console.log("Save not implemented yet.");
+                            }}
+                        >
+                            Save
+                        </Text>
+                        <Text
+                            style={styles.button}
+                            onPress={() => router.back()}>
+                            Back
+                        </Text>
+                    </View>
                 </>
             ) : (
                 <>
                     <Text style={styles.name}>Unknown ingredient.</Text>
-                    <Button title="Back" onPress={() => router.back()}/>
+                    <Text
+                        style={styles.button}
+                        onPress={() => router.back()}>
+                        Back
+                    </Text>
                 </>
             )}
         </View>
@@ -80,33 +165,91 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#000000',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
+        position: 'relative',
+        padding: 10
+    },
+    nameContainer: {
+        height: 150,
+        padding: 10,
+        marginTop: 30,
+        justifyContent: 'center',
+        borderColor: '#ffffff',
+        color: '#ffffff'
     },
     name: {
         fontSize: 24,
         fontWeight: 'bold',
         color: '#ffffff',
-        marginBottom: 10,
+        flex: 1,
+        textAlign: 'center',
+        alignItems: 'center',
+        width: 390,
+        height: 140,
+        borderColor: '#ffffff',
+        borderWidth: 1,
+        backgroundColor: '#141414',
+        borderRadius: 10
+    },
+    imageContainer: {
+        height: 200,
+        marginBottom: 10
     },
     image: {
-        width: 200,
-        height: 200,
-        marginBottom: 10,
+        width: 1000,
+        height: 1000,
         resizeMode: 'contain',
+        flex: 1,
+        alignItems: 'center',
+        borderColor: '#fff',
+    },
+    descriptionContainer: {
+        height: 150,
+        rowGap: 20
     },
     description: {
         fontSize: 16,
         fontWeight: 'normal',
         color: '#ffffff',
-        marginBottom: 10,
-        textAlign: 'center'
+        marginBottom: 5,
+        flex: 1,
+        rowGap: 0,
+        width: 380,
+        height: 1000,
+        borderColor: '#ffffff',
+        borderWidth: 1,
+        backgroundColor: '#141414',
+        borderRadius: 10
+    },
+    nutritionGradeContainer: {
+        height: 150,
+        marginTop: 5,
+        marginBottom: 5
     },
     nutritionGrade: {
-        fontSize: 18,
+        fontSize: 25,
         color: 'green',
         fontWeight: 'bold',
-        marginBottom: 10,
+        flex: 1
+    },
+    buttonContainer: {
+        height: 150,
+        padding: 5
+    },
+    button: {
+       color: '#fff',
+       borderWidth: 1,
+       borderColor: '#ffffff',
+       borderRadius: 10,
+       textAlign: 'center',
+       textAlignVertical: 'center',
+       width: 110,
+       height: 40,
+       bottom: 100,
+       left: 0,
+       right: 0,
+       padding: 10,
     },
     text: {
         fontSize: 18,
